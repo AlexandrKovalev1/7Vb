696e85c58884ac55c1922c88af9c8d1f770d1d9c
alexndrkovalev <yakovalev1@mail.ru>
2024-08-12 14:50:52 +0300

    feat:finish make a home page and prevCard

--- src/components/ui/previewCard/previewCard.tsx
+++ src/components/ui/previewCard/previewCard.tsx
@@ -1,5 +1,28 @@
-import { Card } from '@/components'
+import { Link } from 'react-router-dom'
 
-export const PreviewCard = () => {
-  return <Card></Card>
+import { PATH } from '@/app/router/routes'
+import { Button, Card } from '@/components'
+
+import s from './previewCard.module.scss'
+
+type Props = {
+  cover: string
+  id: string
+  title: string
+  type: string
+}
+
+export const PreviewCard = ({ cover, title, type }: Props) => {
+  return (
+    <Card className={s.wrapper}>
+      <img alt={'cover-img'} className={s.coverImg} src={cover} />
+      <div>
+        <h3>{title}</h3>
+        <span className={s.typeText}>{type}</span>
+      </div>
+      <Button as={Link} className={s.button} to={PATH.LOGIN}>
+        Download
+      </Button>
+    </Card>
+  )
 }
--- src/pages/publicPages/home/home.tsx
+++ src/pages/publicPages/home/home.tsx
@@ -1,5 +1,24 @@
+import testCover from '@/assets/img/assasin.jpeg'
+import { PreviewCard } from '@/components/ui/previewCard'
+
 import s from './home.module.scss'
 
 export const Home = () => {
-  return <div></div>
+  return (
+    <div className={s.wrapper}>
+      <div>
+        <h1 className={s.heading}>Приватные читы 7vb</h1>
+        <p className={s.description}>
+          Интуитивный интерфейс, подсказки по функциям и множество.
+          <br /> Получайте преимущество над игроками без DLC и быстро апайте ранги!
+        </p>
+      </div>
+      <div className={s.prevCardsBlock}>
+        <PreviewCard cover={testCover} id={'1'} title={'Assasin'} type={'Internal'} />
+        <PreviewCard cover={testCover} id={'1'} title={'Assasin'} type={'Internal'} />
+        <PreviewCard cover={testCover} id={'1'} title={'Assasin'} type={'Internal'} />
+        <PreviewCard cover={testCover} id={'1'} title={'Assasin'} type={'Internal'} />
+      </div>
+    </div>
+  )
 }
